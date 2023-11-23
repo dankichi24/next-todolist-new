c16ec7e394e470f57770c2f406488748099ca0d3
dankichi24 <dan24.yukito@gmail.com>
2023-11-23 20:11:03 +0900

    構築～CSS実装

---
+++ components/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <ul className="mx-auto">
+        <div className="flex bg-orange-200 rounded-md mt-2 mb-2 p-2 justify-between">
+          <li className="font-medium">✅ 読書</li>
+          <span className="cursor-pointer">🚮</span>
+        </div>
+        <div className="flex bg-orange-200 rounded-md mt-2 mb-2 p-2 justify-between">
+          <li className="font-medium">✅ 散歩</li>
+          <span className="cursor-pointer">🚮</span>
+        </div>
+        <div className="flex bg-orange-200 rounded-md mt-2 mb-2 p-2 justify-between">
+          <li className="font-medium">✅ トレーニング</li>
+          <span className="cursor-pointer">🚮</span>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
